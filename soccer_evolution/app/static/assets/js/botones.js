f6a67879ec45f5553f56b6c09149a9381b5fb7b1
Luis Montes <101368246+LuisMontesLorca@users.noreply.github.com>
2023-06-03 23:06:38 -0400

    Integracion de modal

--- soccer_evolution/app/static/assets/js/botones.js
+++ soccer_evolution/app/static/assets/js/botones.js
@@ -1,4 +1,4 @@
-var editButton = document.getElementById('edit-button');
+/*var editButton = document.getElementById('edit-button');
 var saveButton = document.getElementById('save-button');
 var fields = document.querySelectorAll('input:not([type="submit"]), select');
 
@@ -23,3 +23,22 @@ function disableFields() {
 // Asignar eventos a los botones
 editButton.addEventListener('click', enableFields);
 saveButton.addEventListener('click', disableFields);
+*/
+
+const openModalButton = document.getElementById('open_modal');
+const closeModalButton = document.getElementById('close_modal');
+const modal = document.getElementById('modal');
+
+openModalButton.addEventListener('click', () => {
+  modal.classList.add('show');
+});
+
+closeModalButton.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.classList.remove('show');
+  }
+});
