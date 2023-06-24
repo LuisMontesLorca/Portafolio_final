16c5d88bd26a4251b941dd433976e528d4c54532
Pris <pr.espinoza@duocuc.cl>
2023-06-24 10:57:46 -0400

    admin

---
+++ soccer_evolution/app/static/assets/js/administrador.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    $('.agregar-cancha-futbol').on('click', function() {
+        $('#modal_agregar_cancha').modal('show');
+
+    $('#modal_agregar_cancha').on('click', '.close, .btn-secondary',function() {
+        // Cerrar la modal
+        $('#modal_agregar_cancha').modal('hide');
+})
+    });
+});
+
+$(document).ready(function() {
+    $('.agregar-cancha-basket').on('click', function() {
+        $('#modal_agregar_cancha').modal('show');
+
+    $('#modal_agregar_cancha').on('click', '.close, .btn-secondary',function() {
+        // Cerrar la modal
+        $('#modal_agregar_cancha').modal('hide');
+})
+    });
+});
+
+$(document).ready(function() {
+    $('.agregar-cancha-tenis').on('click', function() {
+        $('#modal_agregar_cancha').modal('show');
+
+    $('#modal_agregar_cancha').on('click', '.close, .btn-secondary',function() {
+        // Cerrar la modal
+        $('#modal_agregar_cancha').modal('hide');
+})
+    });
+});
+
+$(document).ready(function() {
+    $('.agregar-camiseta').on('click', function() {
+        $('#modal_agregar_cancha').modal('show');
+
+    $('#modal_agregar_cancha').on('click', '.close, .btn-secondary',function() {
+        // Cerrar la modal
+        $('#modal_agregar_cancha').modal('hide');
+})
+    });
+});
+
+$(document).ready(function() {
+    $('.agregar-pelota').on('click', function() {
+        $('#modal_agregar_cancha').modal('show');
+
+    $('#modal_agregar_cancha').on('click', '.close, .btn-secondary',function() {
+        // Cerrar la modal
+        $('#modal_agregar_cancha').modal('hide');
+})
+    });
+});
+
+$(document).ready(function() {
+    $('.agregar-bebida').on('click', function() {
+        $('#modal_agregar_cancha').modal('show');
+
+    $('#modal_agregar_cancha').on('click', '.close, .btn-secondary',function() {
+        // Cerrar la modal
+        $('#modal_agregar_cancha').modal('hide');
+})
+    });
+});
+
+
